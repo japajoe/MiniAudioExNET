dbba1fa79e578387e54a28d85122b80455a3c01c
Wesley <wjapajoe@ziggo.nl>
2025-09-17 22:37:44 -0300

    Fix bug in example

--- examples/Example8.cs
+++ examples/Example8.cs
@@ -8,45 +8,47 @@ namespace MiniAudioExExamples
 {
 	class Example8
 	{
+		private static IntPtr pEngine = IntPtr.Zero;
+		
 		static void Main(string[] args)
 		{
-            MaLog log = new MaLog();
-            log.Message += OnLog;
+			MaLog log = new MaLog();
+			log.Message += OnLog;
 
-            MaContext context = new MaContext(log);
+			MaContext context = new MaContext(log);
 
-            MaDecodingBackendVTable[] vtables = {
-                MaDecodingBackendVTable.CreateFromLibVorbis()
-            };
+			MaDecodingBackendVTable[] vtables = {
+				MaDecodingBackendVTable.CreateFromLibVorbis()
+			};
 
-            MaResourceManager resourceManager = new MaResourceManager(vtables);
+			MaResourceManager resourceManager = new MaResourceManager(vtables);
 
-            MaDeviceInfo deviceInfo = context.GetDefaultPlaybackDevice();
-            ma_device_data_proc deviceDataCallback = OnDeviceData;
-            MaDevice device = new MaDevice(context, deviceInfo, ma_format.f32, 2, 44100, 2048, deviceDataCallback);
+			MaDeviceInfo deviceInfo = context.GetDefaultPlaybackDevice();
+			ma_device_data_proc deviceDataCallback = OnDeviceData;
+			MaDevice device = new MaDevice(context, deviceInfo, ma_format.f32, 2, 44100, 2048, deviceDataCallback);
 
-            MaEngine engine = new MaEngine(device, resourceManager);
-            pEngine = engine.Handle;
+			MaEngine engine = new MaEngine(device, resourceManager);
+			pEngine = engine.Handle;
 
-            device.Start();
+			device.Start();
 
-            MaSound sound = new MaSound(engine);
-            sound.Loop = true;
-            sound.LoadFromFile("some_audio.ogg", true);
-            
-            sound.Play();
-            Console.ReadLine();
+			MaSound sound = new MaSound(engine);
+			sound.Loop = true;
+			sound.LoadFromFile("some_audio.ogg", true);
 
-            sound.Stop();
-            device.Stop();
+			sound.Play();
+			Console.ReadLine();
 
-            // Dispose in the reverse order of initialization
-            sound.Dispose();
-            engine.Dispose();
-            resourceManager.Dispose();
-            device.Dispose();
-            context.Dispose();
-            log.Dispose();
+			sound.Stop();
+			device.Stop();
+
+			// Dispose in the reverse order of initialization
+			sound.Dispose();
+			engine.Dispose();
+			resourceManager.Dispose();
+			device.Dispose();
+			context.Dispose();
+			log.Dispose();
 		}
 		
         private static void OnDeviceData(ma_device_ptr pDevice, IntPtr pOutput, IntPtr pInput, UInt32 frameCount)
